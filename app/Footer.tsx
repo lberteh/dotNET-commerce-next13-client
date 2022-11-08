f438a7e1849447038fd28d679361e26301399297
Lucas Schoenardie <lucas@Camilas-MacBook-Pro.local>
2022-11-08 18:10:25 -0500

    build: implement prettier and configure eslint

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
       <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-        © 2022{" "}
+        © 2022{' '}
         <a href="https://flowbite.com/" className="hover:underline">
           Flowbite™
         </a>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import Footer from "./Footer";
-import "./globals.css";
-import Header from "./Header";
+import Footer from './Footer';
+import './globals.css';
+import Header from './Header';
 
 export default function RootLayout({
   children,
