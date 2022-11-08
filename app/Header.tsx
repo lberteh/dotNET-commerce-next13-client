3645325975b81a797ad4fabef607e11245f7d03e
Lucas Schoenardie <lucas@Camilas-MacBook-Pro.local>
2022-11-08 10:47:08 -0500

    first commit

---
+++ app/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1 className="w-full p-4 bg-blue-400">HEADER</h1>
+    </header>
+  );
+};
+
+export default Header;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
-import './globals.css'
+import Footer from "./Footer";
+import "./globals.css";
+import Header from "./Header";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +14,11 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
