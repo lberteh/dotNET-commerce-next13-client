df876bb198956f71328f1ba65d1a1f37f68cb257
Lucas Schoenardie <lucas@Camilas-MacBook-Pro.local>
2022-11-11 17:12:45 -0500

    feat: implement @next/font and add font 'inter'

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import Footer from './Footer';
 import './globals.css';
 import Header from './Header';
+import { Inter } from '@next/font/google';
+
+const inter = Inter();
 
 export default function RootLayout({
   children,
@@ -8,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
